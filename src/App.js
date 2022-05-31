56869d86d15a25407cc4043d2347b0b29a82e5fb
Noah <crashlive112@gmail.com>
2022-05-30 22:04:17 -0400

    import useState and useEffect hooks into App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 import DestinationsList from './Components/DestinationsList';
 
 function App() {
