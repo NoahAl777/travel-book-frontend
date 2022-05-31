532106c1977295373a34e90eebaeb6bb55d27709
Noah <crashlive112@gmail.com>
2022-05-30 22:30:37 -0400

    create NotesList component

---
+++ src/Components/NotesList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotesList = () => {
+
+  return (
+    <div className="NotesList">
+      <h4>NotesList Component</h4>
+    </div>
+  )
+};
+
+export default NotesList;
