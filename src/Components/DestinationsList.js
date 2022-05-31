04325dc61d60a6c0228c26ecbacecfd582c66ba1
Noah <crashlive112@gmail.com>
2022-05-30 21:59:20 -0400

    create DestinationsList Component

---
+++ src/Components/DestinationsList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DestinationsList = () => {
+
+  retun(
+    <div className="DestinationsList">
+      <h2>DestinationsList Component</h2>
+    </div>
+  )
+};
+
+export default DestinationsList;
