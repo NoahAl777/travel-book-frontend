f1a17fbde8c6ab6b275e7121ac87d497aad0a814
Noah <crashlive112@gmail.com>
2022-05-30 22:54:04 -0400

    add more information to note cards

--- src/Components/NotesList.js
+++ src/Components/NotesList.js
@@ -6,6 +6,8 @@ const NotesList = ({ notes }) => {
     return (
       <div className="noteCard" key={note.id}>
         <h4>{`🙌${note.overall_rating} - Overall | 🥧${note.food_rating} - Food | 👮‍♂️${note.safety_rating} - Safety`}</h4>
+        <p>Must do: {`${note.must_do}`}</p>
+        <p>More: {`${note.additional_notes}`}</p>
       </div>
     )
   })
