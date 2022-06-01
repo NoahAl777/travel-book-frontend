6f050a44c9d6a3e897a0e52000f7483242332118
Noah <crashlive112@gmail.com>
2022-05-31 21:02:54 -0400

    create DestinationForm component

---
+++ src/Components/DestinationForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DestinationForm = () => {
+
+  return (
+    <div className="DestinationForm">
+      Destination Form Component
+    </div>
+  )
+};
+
+export default DestinationForm;
