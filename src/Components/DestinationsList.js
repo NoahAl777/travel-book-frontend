f6bb75fa9818d82fcf057c677ad3c916434380a6
Noah <crashlive112@gmail.com>
2022-05-31 20:40:05 -0400

    add zipcode to Destination cards

--- src/Components/DestinationsList.js
+++ src/Components/DestinationsList.js
@@ -6,7 +6,7 @@ const DestinationsList = ({ destinations }) => {
   let destinationCards = destinations.map(destination => {
     return (
       <div className="DestinationCard" key={destination.id}>
-        <h2>{`${destination.city}, ${destination.country} - ${destination.state_province}`}</h2>
+        <h2>{`${destination.city}, ${destination.country} - ${destination.state_province}  (${destination.zipcode})`}</h2>
         <img src={`${destination.image}`} width="1200" height="800"></img>
         <NotesList notes={destination.notes} />
       </div>
