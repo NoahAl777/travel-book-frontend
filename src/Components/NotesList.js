2d1b3c53cae247e8a8aed767dcfc08ee7c6ade0a
Noah <crashlive112@gmail.com>
2022-05-31 22:03:28 -0400

    remove Link from NotesList

--- src/Components/NotesList.js
+++ src/Components/NotesList.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const NotesList = ({ notes }) => {
-  console.log(notes)
   const noteCards = notes.map(note => {
     return (
       <div className="noteCard" key={note.id}>
