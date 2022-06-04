36faeda78d38578454eb02be74ff0a704dd3234d
Noah <crashlive112@gmail.com>
2022-06-04 13:06:11 -0400

    create EditDestinationsForm component

---
+++ src/Components/EditDestinationsForm.js
@@ -0,0 +1,12 @@
+import React from "react;"
+
+const EditDestinationsForm = () => {
+
+  return (
+    <div className="EditDestinationsForm">
+      Edit Destinations Form Component
+    </div>
+  )
+};
+
+export default EditDestinationsForm;
