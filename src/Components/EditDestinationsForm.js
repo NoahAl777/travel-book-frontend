ae583cb262f59e9284798b48b8ea7e0c8fc64961
Noah <crashlive112@gmail.com>
2022-06-04 13:48:45 -0400

    add handleChange function to form inputs

--- src/Components/EditDestinationsForm.js
+++ src/Components/EditDestinationsForm.js
@@ -5,6 +5,10 @@ const EditDestinationsForm = ({ destinations }) => {
   const params = useParams()
   const [selectedDestination, setSelectedDestination] = useState(destinations.filter((destination) => destination.id == params.destination_id))
 
+  const handleChange = (event) => {
+    console.log(event)
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault()
   };
@@ -13,19 +17,19 @@ const EditDestinationsForm = ({ destinations }) => {
     <div className="EditDestinationsForm">
       <form onSubmit={handleSubmit}>
         <label>Country:</label>
-        <input type="text" id="country" value={selectedDestination[0].country}></input>
+        <input type="text" id="country" onChange={handleChange} value={selectedDestination[0].country}></input>
         <br />
         <label>City:</label>
-        <input type="text" id="city" value={selectedDestination[0].city}></input>
+        <input type="text" id="city" onChange={handleChange} value={selectedDestination[0].city}></input>
         <br />
         <label>State/Province:</label>
-        <input type="text" id="state_province" value={selectedDestination[0].state_province}></input>
+        <input type="text" id="state_province" onChange={handleChange} value={selectedDestination[0].state_province}></input>
         <br />
         <label>Zipcode:</label>
-        <input type="integer" id="zipcode" value={selectedDestination[0].zipcode}></input>
+        <input type="integer" id="zipcode" onChange={handleChange} value={selectedDestination[0].zipcode}></input>
         <br />
         <label>Image URL:</label>
-        <input type="text" id="image" value={selectedDestination[0].image}></input>
+        <input type="text" id="image" onChange={handleChange} value={selectedDestination[0].image}></input>
         <br />
         <input type="submit"></input>
       </form>
