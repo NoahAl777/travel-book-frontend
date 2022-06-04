54da2594f7f41820a7b0027c7c8c8aeac1adffa3
Noah <crashlive112@gmail.com>
2022-06-04 16:23:09 -0400

    add a form by using previous form as a template

--- src/Components/EditDestinationsForm.js
+++ src/Components/EditDestinationsForm.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const EditDestinationsForm = ({ selectedDestination }) => {
   const { country, state_province, city, zipcode, image } = selectedDestination
+  const [formData, setFormData] = useState({ country: country, state_province: state_province, city: city, zipcode: zipcode, image: image })
+
+  const handleChange = (event) => {
+    console.log(formData)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+  }
 
   return (
     <div className="EditDestinationsForm">
-      Edit Destinations Form Component
+      <form onSubmit={handleSubmit}>
+        <label>Country:</label>
+        <input type="text" id="country" onChange={handleChange} value={formData.country}></input>
+        <br />
+        <label>City:</label>
+        <input type="text" id="city" onChange={handleChange} value={formData.city}></input>
+        <br />
+        <label>State/Province:</label>
+        <input type="text" id="state_province" onChange={handleChange} value={formData.state_province}></input>
+        <br />
+        <label>Zipcode:</label>
+        <input type="integer" id="zipcode" onChange={handleChange} value={formData.zipcode}></input>
+        <br />
+        <label>Image URL:</label>
+        <input type="text" id="image" onChange={handleChange} value={formData.image}></input>
+        <br />
+        <input type="submit"></input>
+      </form>
     </div>
   )
 };
