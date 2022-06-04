6ad9b58bed82426614bb2b9848bed7ffd9d80081
Noah <crashlive112@gmail.com>
2022-06-04 13:33:49 -0400

    on load setSelectedDestination to the destination indicated by the url with useEffect

--- src/Components/EditDestinationsForm.js
+++ src/Components/EditDestinationsForm.js
@@ -2,8 +2,13 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const EditDestinationsForm = ({ destinations }) => {
-  const paramas = useParams()
+  const params = useParams()
   const [selectedDestination, setSelectedDestination] = useState({})
+
+  useEffect(() => {
+    setSelectedDestination(destinations.filter((destination) => destination.id == params.destination_id))
+  }, [])
+  console.log(selectedDestination)
 
   return (
     <div className="EditDestinationsForm">
