e00774619ce616fa5a1498eaa85aa70fb8fcc445
Noah <crashlive112@gmail.com>
2022-06-05 05:26:16 -0400

    create EditNotesForm component

---
+++ src/Components/EditNotesForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const EditNotesForm = () => {
+
+  return (
+    <div className="EditNotesForm">
+      Edit Notes Form Component
+    </div>
+  )
+};
+
+export default EditNotesForm;
