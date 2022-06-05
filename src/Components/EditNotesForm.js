a8aa33140d160ab6ef2cb6693080b1124a9d2ff2
Noah <crashlive112@gmail.com>
2022-06-05 05:59:26 -0400

    add form by using new form as template

--- src/Components/EditNotesForm.js
+++ src/Components/EditNotesForm.js
@@ -3,10 +3,35 @@ import React, { useState } from "react";
 const EditNotesForm = ({ selectedNote }) => {
   const { id, overall_rating, safety_rating, food_rating, must_do, additional_notes } = selectedNote
   const [formData, setFormData] = useState({ overall_rating: overall_rating, safety_rating: safety_rating, food_rating: food_rating, must_do: must_do, additional_notes: additional_notes })
-  console.log(formData)
+
+  const handleChange = () => {
+
+  }
+
+  const handleSubmit = (event) => {
+
+  }
+
   return (
     <div className="EditNotesForm">
-      Edit Notes Form Component
+      <form onSubmit={handleSubmit}>
+        <label>Overall Rating:</label>
+        <input type="number" id="overall_rating" onChange={handleChange} value={formData.overall_rating}></input>
+        <br />
+        <label>Safety Rating:</label>
+        <input type="number" id="safety_rating" onChange={handleChange} value={formData.safety_rating}></input>
+        <br />
+        <label>Food Rating:</label>
+        <input type="number" id="food_rating" onChange={handleChange} value={formData.food_rating}></input>
+        <br />
+        <label>Must Do:</label>
+        <input type="text" id="must_do" onChange={handleChange} value={formData.must_do}></input>
+        <br />
+        <label>More:</label>
+        <input type="text" id="additional_notes" onChange={handleChange} value={formData.additional_notes}></input>
+        <br />
+        <input type="submit"></input>
+      </form>
     </div>
   )
 };
