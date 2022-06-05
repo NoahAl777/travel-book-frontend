d7f94a1e7d7e19853bcb7f51175c359eee7e5acf
Noah <crashlive112@gmail.com>
2022-06-05 05:56:33 -0400

    create formData state and set its default state to the selectedNote

--- src/Components/EditNotesForm.js
+++ src/Components/EditNotesForm.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 const EditNotesForm = ({ selectedNote }) => {
-
+  const { id, overall_rating, safety_rating, food_rating, must_do, additional_notes } = selectedNote
+  const [formData, setFormData] = useState({ overall_rating: overall_rating, safety_rating: safety_rating, food_rating: food_rating, must_do: must_do, additional_notes: additional_notes })
+  console.log(formData)
   return (
     <div className="EditNotesForm">
       Edit Notes Form Component
