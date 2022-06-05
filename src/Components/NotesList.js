d4d74ddb996dfe0f95cccba17f5adc5ce19c8d91
Noah <crashlive112@gmail.com>
2022-06-05 05:38:41 -0400

    add Edit Note button to note cards

--- src/Components/NotesList.js
+++ src/Components/NotesList.js
@@ -3,11 +3,15 @@ import { Link } from "react-router-dom";
 
 const NotesList = ({ notes }) => {
   const noteCards = notes.map(note => {
+    console.log(note)
     return (
       <div className="noteCard" key={note.id}>
         <h4>{`🙌${note.overall_rating} - Overall | 🥧${note.food_rating} - Food | 👮‍♂️${note.safety_rating} - Safety`}</h4>
         <p>Must do: {`${note.must_do}`}</p>
         <p>More: {`${note.additional_notes}`}</p>
+        <Link to={`/destinations/${note.destination_id}/notes/${note.id}/edit`}>
+          <button>Edit Note</button>
+        </Link>
       </div>
     )
   })
