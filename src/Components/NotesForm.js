51922004ce8adc851d5ace8894880ceb629f9e63
Noah <crashlive112@gmail.com>
2022-06-12 23:03:23 -0400

    add title to NotesForm

--- src/Components/NotesForm.js
+++ src/Components/NotesForm.js
@@ -40,27 +40,30 @@ const NotesForm = ({ destinations, setDestinations, notes, setNotes }) => {
   }
 
   return (
-    <div className="NotesForm">
-      <h4 style={{ color: 'red' }}>{errors}</h4>
-      <form onSubmit={handleSubmit}>
-        <label>Overall Rating:</label>
-        <input type="number" id="overall_rating" onChange={handleChange} value={formData.overall_rating}></input>
-        <br />
-        <label>Safety Rating:</label>
-        <input type="number" id="safety_rating" onChange={handleChange} value={formData.safety_rating}></input>
-        <br />
-        <label>Food Rating:</label>
-        <input type="number" id="food_rating" onChange={handleChange} value={formData.food_rating}></input>
-        <br />
-        <label>Must Do:</label>
-        <input type="text" id="must_do" onChange={handleChange} value={formData.must_do}></input>
-        <br />
-        <label>Additional Notes:</label>
-        <input type="text" id="additional_notes" onChange={handleChange} value={formData.additional_notes}></input>
-        <br />
-        <input type="submit"></input>
-      </form>
-    </div>
+    <>
+      <h2>Create New Note</h2>
+      <div className="NotesForm">
+        <h4 style={{ color: 'red' }}>{errors}</h4>
+        <form onSubmit={handleSubmit}>
+          <label>Overall Rating:</label>
+          <input type="number" id="overall_rating" onChange={handleChange} value={formData.overall_rating}></input>
+          <br />
+          <label>Safety Rating:</label>
+          <input type="number" id="safety_rating" onChange={handleChange} value={formData.safety_rating}></input>
+          <br />
+          <label>Food Rating:</label>
+          <input type="number" id="food_rating" onChange={handleChange} value={formData.food_rating}></input>
+          <br />
+          <label>Must Do:</label>
+          <input type="text" id="must_do" onChange={handleChange} value={formData.must_do}></input>
+          <br />
+          <label>Additional Notes:</label>
+          <input type="text" id="additional_notes" onChange={handleChange} value={formData.additional_notes}></input>
+          <br />
+          <input type="submit"></input>
+        </form>
+      </div>
+    </>
   )
 };
 
