b1b90eb7c5ac259909ea4859ccc9d6bf9378f69a
Noah <crashlive112@gmail.com>
2022-06-12 22:28:07 -0400

    add error handling and message display to EditNotesForm by using EditDestinationsForm as template

--- src/Components/EditNotesForm.js
+++ src/Components/EditNotesForm.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
-const EditNotesForm = ({ selectedNote }) => {
+const EditNotesForm = ({ selectedNote, notes, setNotes }) => {
   const params = useParams();
+  const navigate = useNavigate();
   const { id, overall_rating, safety_rating, food_rating, must_do, additional_notes, destination_id } = selectedNote;
   const [formData, setFormData] = useState({ overall_rating: overall_rating, safety_rating: safety_rating, food_rating: food_rating, must_do: must_do, additional_notes: additional_notes });
   const [errors, setErrors] = useState([])
@@ -32,11 +33,29 @@ const EditNotesForm = ({ selectedNote }) => {
       body: JSON.stringify(formData)
     })
       .then(r => r.json())
-      .then(data => console.log(data))
+      .then(data => handleUpdateNotesList(data))
+  }
+
+  const handleUpdateNotesList = (data) => {
+    if (data.errors != undefined) {
+      setErrors(data.errors)
+      console.log(data.errors)
+    } else {
+      setNotes(notes.map(note => {
+        if (note.id == data.id) {
+          return data
+        } else {
+          return note
+        }
+      }))
+      setErrors()
+      navigate("/")
+    }
   }
 
   return (
     <div className="EditNotesForm">
+      <h4 style={{ color: 'red' }}>{errors}</h4>
       <form onSubmit={handleSubmit}>
         <label>Overall Rating:</label>
         <input type="number" id="overall_rating" onChange={handleChange} value={formData.overall_rating}></input>
