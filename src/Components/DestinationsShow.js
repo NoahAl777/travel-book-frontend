2c2241e78022964ed0f57399604331bc5f2ea823
Noah <crashlive112@gmail.com>
2022-06-23 12:05:58 -0400

    import useParams hook

--- src/Components/DestinationsShow.js
+++ src/Components/DestinationsShow.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const DestinationsShow = () => {
-
+  const params = useParams()
+  console.log(params)
   return (
     <div className='DestinationsShow'>
 
