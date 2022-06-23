2dc3e8f7fe253c3dd8ba9dc66c3b4a2eb74d361e
Noah <crashlive112@gmail.com>
2022-06-23 11:55:23 -0400

    create and display destination cards

--- src/Components/DestinationsIndex.js
+++ src/Components/DestinationsIndex.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 const DestinationsIndex = ({ destinations }) => {
+
+  const destinationsToDisplay = destinations.map(destination => {
+    return (
+      <div className='DestinationCard' key={destination.id}>
+        <img src={`${destination.image}`} width="200" height="150"></img>
+        <p>{`${destination.city}, ${destination.country} - ${destination.state_province}  (${destination.zipcode})`}</p>
+        <br />
+      </div>)
+  })
+
   return (
     <div className='DestinationsIndex'>
-
+      {destinationsToDisplay}
     </div>
   )
 };
