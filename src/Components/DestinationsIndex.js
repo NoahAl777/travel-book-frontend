958aa7ad144a67af82ef2cea9e96cf0a7bed2534
Noah <crashlive112@gmail.com>
2022-06-23 11:59:00 -0400

    add Link to redirect to destinations show page on click

--- src/Components/DestinationsIndex.js
+++ src/Components/DestinationsIndex.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const DestinationsIndex = ({ destinations }) => {
 
   const destinationsToDisplay = destinations.map(destination => {
     return (
       <div className='DestinationCard' key={destination.id}>
-        <img src={`${destination.image}`} width="200" height="150"></img>
-        <p>{`${destination.city}, ${destination.country} - ${destination.state_province}  (${destination.zipcode})`}</p>
+        <Link to={`/destinations/${destination.id}`}>
+          <img src={`${destination.image}`} width="200" height="150"></img>
+          <p>{`${destination.city}, ${destination.country} - ${destination.state_province}  (${destination.zipcode})`}</p>
+        </Link>
         <br />
       </div>)
   })
